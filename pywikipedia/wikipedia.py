hroest/pywikipedia-git

commit b124cd2bcd711644da87fb1d572dfdf159560496

handleArgs() bug and aslink() formatting
git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk@6249 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

pywikipedia/wikipedia.py
@@ -586,12 +586,13 @@ not supported by PyWikipediaBot!"""
         if not noInterwiki and (forceInterwiki or self.site() != getSite()):
             if self.site().family != getSite().family \
                     and self.site().family.name != self.site().lang:
-                return u'[[%s:%s:%s]]' % (self.site().family.name,
-                                          self.site().lang,
-                                          self.title(savetitle=True,decode=True))
+                return u'[[:%s:%s:%s]]' % (self.site().family.name,
+                                           self.site().lang,
+                                           self.title(savetitle=True,
+                                                      decode=True))
             else:
-                return u'[[%s:%s]]' % (self.site().lang,
-                                       self.title(savetitle=True,decode=True))
+                return u'[[:%s:%s]]' % (self.site().lang,
+                                        self.title(savetitle=True, decode=True))
         elif textlink and (self.isImage() or self.isCategory()):
                 return u'[[:%s]]' % self.title(savetitle=True, decode=True)
         else:
@@ -6194,14 +6195,14 @@ def handleArgs(*args):
     global default_code, default_family, verbose
     # get commandline arguments
     if not args:
-        args = sys.argv
+        args = sys.argv[1:]
     # get the name of the module calling this function. This is
     # required because the -help option loads the module's docstring and because
     # the module name will be used for the filename of the log.
     # TODO: check if the following line is platform-independent
     moduleName = calledModuleName()
     nonGlobalArgs = []
-    for arg in args[1:]:
+    for arg in args:
         arg = decodeArg(arg)
         if arg == '-help':
             showHelp(moduleName)

commit 1920b65bc52726d47cfc661a624a010166e4d830

Interwiki bugfix for bug introduced in r6249
git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk@6250 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

pywikipedia/wikipedia.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8  -*-
+﻿# -*- coding: utf-8  -*-
 """
 Library to get and put pages on a MediaWiki.
 
@@ -3643,7 +3643,7 @@ def interwikiFormat(links, insite = None):
     s = []
     for site in ar:
         try:
-            link = links[site].aslink(forceInterwiki=True)
+            link = links[site].aslink(forceInterwiki=True).replace('[[:', '[[')
             s.append(link)
         except AttributeError:
             s.append(getSite(site).linkto(links[site], othersite=insite))