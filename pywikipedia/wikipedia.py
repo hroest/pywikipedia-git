0199dcc9a7f3f50773f0901742b300bf999a2faf
xqt <xqt@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2010-06-11 09:56:33 +0000

    cosmetic changes due to r8027
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk@8277 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- pywikipedia/wikipedia.py
+++ pywikipedia/wikipedia.py
@@ -4090,8 +4090,8 @@ def getall(site, pages, throttle=True, force=False):
     """
     # TODO: why isn't this a Site method?
     pages = list(pages)  # if pages is an iterator, we need to make it a list
-    output(u'Getting %d pages %s from %s...'
-           % (len(pages), iif(site.has_api() and debug, u'via API', u''), site))
+    output(u'Getting %d pages %sfrom %s...'
+           % (len(pages), iif(site.has_api() and debug, u'via API ', u''), site))
     limit = config.special_page_limit / 4 # default is 500/4, but It might have good point for server.
     if len(pages) > limit:
         # separate export pages for bulk-retrieve
