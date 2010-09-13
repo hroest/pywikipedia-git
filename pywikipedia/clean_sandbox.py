b3e844b11e521298264f7160ca24c7435eb7f1ff
xqt <xqt@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2010-09-13 15:49:35 +0000

    bugfix for wrong minutesDiff
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk@8551 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- pywikipedia/clean_sandbox.py
+++ pywikipedia/clean_sandbox.py
@@ -135,8 +135,12 @@ class SandboxBot:
                 time1 = str(time1)
             if type(time2) is long:
                 time2 = str(time2)
-            t1 = (((int(time1[0:4])*12+int(time1[4:6]))*30+int(time1[6:8]))*24+int(time1[8:10])*60)+int(time1[10:12])
-            t2 = (((int(time2[0:4])*12+int(time2[4:6]))*30+int(time2[6:8]))*24+int(time2[8:10])*60)+int(time2[10:12])
+            t1 = (((int(time1[0:4]) * 12 + int(time1[4:6])) * 30 +
+                   int(time1[6:8])) * 24 + int(time1[8:10])) * 60 + \
+                   int(time1[10:12])
+            t2 = (((int(time2[0:4]) * 12 + int(time2[4:6])) * 30 +
+                   int(time2[6:8])) * 24 + int(time2[8:10])) * 60 + \
+                   int(time2[10:12])
             return abs(t2-t1)
 
         mySite = pywikibot.getSite()
