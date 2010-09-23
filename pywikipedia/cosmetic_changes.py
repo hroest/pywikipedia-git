43152febe2df1bb09753b52ec94d0f51ef42cc8c
xqt <xqt@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2010-09-23 16:46:30 +0000

    changing digits on ckb-wiki disabled for now. There are some problems with picture sizes
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk@8593 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- pywikipedia/cosmetic_changes.py
+++ pywikipedia/cosmetic_changes.py
@@ -727,8 +727,9 @@ class CosmeticChangesToolkit:
             # do not change digits inside html-tags
             pattern = re.compile(u'<[/]*?[^</]+?[/]*?>', re.UNICODE)
             exceptions.append(pattern)
-            for i in range(0,10):
-                text = pywikibot.replaceExcept(text, str(i), u'٠١٢٣٤٥٦٧٨٩'[i], exceptions)
+## disabled for now: some problems with wring picture size
+##            for i in range(0,10):
+##                text = pywikibot.replaceExcept(text, str(i), u'٠١٢٣٤٥٦٧٨٩'[i], exceptions)
         return text
 
     # Retrieved from "http://commons.wikimedia.org/wiki/Commons:Tools/pywiki_file_description_cleanup"
