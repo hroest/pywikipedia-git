3773b4eebb1fadc56906457178c9fc31fe140d57
xqt <xqt@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2010-10-18 16:18:21 +0000

    correct sorting order for nds-wiki: http://nds.wikipedia.org/wiki/Wikipedia:Ik_bruuk_H%C3%BClp/Archiv/2007#Interwiki-Lenken
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk@8666 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- pywikipedia/families/wikipedia_family.py
+++ pywikipedia/families/wikipedia_family.py
@@ -994,7 +994,7 @@ class Family(family.Family):
             'lb': self.alphabetic,
             'mk': self.alphabetic,
             'ms': self.alphabetic_revised,
-            'nds': ['nds-nl', 'pdt'] + self.alphabetic, # Note: as of 2008-02-24, pdt: (Plautdietsch) is still in the Incubator.
+            'nds': ['nds-nl', 'pdt'], # Note: as of 2008-02-24, pdt: (Plautdietsch) is still in the Incubator.
             'nn': ['no', 'nb', 'sv', 'da'] + self.alphabetic,
             'no': self.alphabetic,
             'pl': self.alphabetic,
