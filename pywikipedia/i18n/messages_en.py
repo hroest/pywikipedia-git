976080bf1244a409340c5b7a7e8e90146198d923
multichill <multichill@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2010-10-23 17:14:32 +0000

    Test with i18n
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk@8684 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

---
+++ pywikipedia/i18n/messages_en.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8  -*-
+"""
+Just a test to see if we can get translations to translatewiki
+"""
+#
+# (C) Pywikipedia bot team, 2007-2010
+#
+# Distributed under the terms of the MIT license.
+#
+__version__ = '$Id$'
+
+msg['en'] = {
+    u'interwiki-robot-adding' : u'Robot adding: %s',
+    u'interwiki-robot-modifying' : u'Robot modifying: %s',
+    u'interwiki-robot-removing' : u'Robot removing: %s',
+    }
