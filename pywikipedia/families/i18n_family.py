aa2a894a07bcdaebf7031a06003e8126700572d1
siebrand <siebrand@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2011-01-11 13:24:20 +0000

    Update namespaces and MediaWiki version for i18n_family.py.
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk@8823 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- pywikipedia/families/i18n_family.py
+++ pywikipedia/families/i18n_family.py
@@ -159,6 +159,18 @@ class Family(family.Family):
         self.namespaces[1231] = {
             '_default': [u'Mifos talk'],
         }
+        self.namespaces[1232] = {
+            '_default': [u'Wikia'],
+        }
+        self.namespaces[1233] = {
+            '_default': [u'Wikia talk'],
+        }
+        self.namespaces[1234] = {
+            '_default': [u'OpenImages'],
+        }
+        self.namespaces[1235] = {
+            '_default': [u'OpenImages talk'],
+        }
 
     def version(self, code):
-        return "1.17alpha"
+        return "1.18alpha"
