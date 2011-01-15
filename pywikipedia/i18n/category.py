592e5318afaa91d4b5320792ab3a72f200e9b0e6
roberthl <roberthl@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2011-01-15 00:13:37 +0000

    Add new localisation files by valhallasw.
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk@8826 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

---
+++ pywikipedia/i18n/category.py
@@ -0,0 +1,758 @@
+# -*- coding: utf-8 -*-
+msg = {
+'en': {
+'category-adding': u'Robot: Adding [[Category:%(newcat)s]]',
+'category-changing': u'Robot: Changing %(oldcat)s',
+'category-replacing': u'Robot: Replacing category %(oldcat)s with %(newcat)s',
+'category-was-moved': u'Robot: Category was moved to [[:Category:%(newcat)|%(title)s]]',
+'category-listifying': u'Bot: Listifying from %(fromcat) (%(num)d entries)',
+'category-removing': u'Bot: Removing from %(oldcat)',
+'category-was-disbanded': u'Bot: Category was disbanded',
+'category-also-in': u'(also in %(alsocat))',
+},
+'de': {
+'category-adding': u'Bot: Ergänze [[Kategorie:%(newcat)s]]',
+'category-changing': u'Bot: Ändere %(oldcat)s zu %(newcat)s',
+'category-replacing': u'Bot: Ersetze Kategorie %(oldcat)s durch %(newcat)s',
+'category-was-moved': u'Bot: Kategorie wurde nach [[:Kategorie:%(newcat)|%(title)s]] verschoben',
+'category-removing': u'Bot: Entferne aus %(oldcat)',
+'category-was-disbanded': u'Bot: Kategorie wurde aufgelöst',
+'category-also-in': u'(auch in %(alsocat))',
+},
+'fr': {
+'category-adding': u'Robot : ajoute [[Catégorie:%(newcat)s]]',
+'category-changing': u'Robot : modifie [[%(oldcat)s]]',
+'category-was-moved': u'Robot : catégorie déplacée sur [[:Category:%(newcat)|%(title)s]]',
+'category-removing': u'Robot : Retiré depuis %(oldcat)',
+'category-also-in': u'(également dans %(alsocat))',
+},
+'pl': {
+'category-adding': u'Robot dodaje [[Kategoria:%(newcat)s]]',
+'category-changing': u'Robot przenosi %(oldcat)s',
+'category-was-moved': u'Robot przenosi kategorię do [[:Category:%(newcat)|%(title)s]]',
+'category-listifying': u'Robot: listuje kategorię %(fromcat) (%(num)d stron)',
+'category-removing': u'Robot: Usuwa z kategorii %(oldcat)',
+'category-was-disbanded': u'Robot: Kategoria została usunięta',
+'category-also-in': u'(również w %(alsocat))',
+},
+'it': {
+'category-adding': u'Bot: Aggiungo [[Categoria:%(newcat)s]]',
+'category-changing': u'Bot: Modifico %(oldcat)s',
+'category-was-moved': u'Bot: La categoria è stata sostituita da [[:Categoria:%(newcat)|%(title)s]]',
+},
+'ja': {
+'category-adding': u'ロボットによる: カテゴリ追加 [[Category:%(newcat)s]]',
+'category-changing': u'ロボットによる: カテゴリ変更 [[%(oldcat)s]]→[[%(newcat)s]]',
+'category-replacing': u'ロボットによる: カテゴリ変更 [[%(oldcat)s]]→[[%(newcat)s]]',
+'category-was-moved': u'ロボットによる: カテゴリ [[:Category:%(newcat)]]へ移動',
+'category-removing': u'ロボットによる:[[%(oldcat)]]を除去',
+},
+'es': {
+'category-adding': u'Bot: Añadida [[Categoría:%(newcat)s]]',
+'category-changing': u'Bot: Cambiada %(oldcat)s',
+'category-was-moved': u'Robot: La categoría ha sido movida a [[:Category:%(newcat)|%(title)s]]',
+'category-removing': u'Bot: Eliminada de la %(oldcat)',
+'category-was-disbanded': u'Robot: La categoría ha sido eliminada',
+'category-also-in': u'(también en %(alsocat))',
+},
+'pt': {
+'category-adding': u'Bot: Adicionando [[Categoria:%(newcat)s]]',
+'category-changing': u'Bot: Modificando [[%(oldcat)s]]',
+'category-was-moved': u'Bot: Categoria [[:Category:%(newcat)|%(title)s]] foi movida',
+'category-listifying': u'Bot: Listando de %(fromcat) (%(num)d entradas)',
+'category-removing': u'Bot: Removendo [[%(oldcat)]]',
+'category-was-disbanded': u'Bot: Categoria foi unida',
+'category-also-in': u'(também em %(alsocat))',
+},
+'nl': {
+'category-adding': u'Bot: [[categorie:%(newcat)s]] toegevoegd',
+'category-changing': u'Bot: wijziging %(oldcat)s',
+'category-was-moved': u'Bot: Categorie is hernoemd naar [[:Category:%(newcat)|%(title)s]]',
+'category-listifying': u'Bot: Lijst van %(fromcat) (%(num)d pagina\'s)',
+'category-removing': u'Bot: Verwijderd uit %(oldcat)',
+'category-was-disbanded': u'Bot: Categorie is opgeheven',
+'category-also-in': u'(ook in %(alsocat))',
+},
+'ru': {
+'category-adding': u'Робот: добавление [[Категория:%(newcat)s]]',
+'category-changing': u'Робот: изменение %(oldcat)s',
+'category-was-moved': u'Робот: категория переименована в [[:Категория:%(newcat)|%(title)s]]',
+'category-removing': u'Робот: исключение из %(oldcat)',
+'category-was-disbanded': u'Робот: категория расформирована',
+'category-also-in': u'(также в %(alsocat))',
+},
+'sv': {
+'category-adding': u'Robot: Lägger till [[Kategori:%(newcat)s]]',
+'category-changing': u'Robot: Ändrar %(oldcat)s',
+'category-was-moved': u'Robot: Kategori flyttades till [[:Category:%(newcat)|%(title)s]]',
+'category-listifying': u'Robot: Skapar en lista från %(fromcat) (%(num)d)',
+'category-removing': u'Robot: Tar bort från %(oldcat)',
+'category-was-disbanded': u'Robot: Kategorin upplöstes',
+'category-also-in': u'(också i %(alsocat))',
+},
+'zh': {
+'category-adding': u'機器人:新增目錄 [[Category:%(newcat)s]]',
+'category-changing': u'機器人:變更目錄 [[%(oldcat)s]]→[[%(newcat)s]]',
+'category-replacing': u'機器人:變更目錄 [[%(oldcat)s]]→[[%(newcat)s]]',
+'category-was-moved': u'機器人:移動目錄至 [[:Category:%(newcat)|%(title)s]]',
+'category-listifying': u'機器人: 從%(fromcat)提取列表(%(num)d個項目)',
+'category-removing': u'機器人:移除目錄 [[%(oldcat)]]',
+'category-was-disbanded': u'機器人:本目錄已解散',
+'category-also-in': u'(也在 %(alsocat))',
+},
+'ca': {
+'category-adding': u'Robot: Afegint [[Categoria:%(newcat)s]]',
+'category-changing': u'Robot: Canviant %(oldcat)s',
+'category-was-moved': u'Robot: La categoria s\'ha mogut a [[:Categoria:%(newcat)|%(title)s]]',
+'category-listifying': u'Robot: Llistant de %(fromcat) (%(num)d entrades)',
+'category-removing': u'Robot: Eliminant de %(oldcat)',
+'category-was-disbanded': u'Robot: La categoria s\'ha eliminat',
+'category-also-in': u'(també a %(alsocat))',
+},
+'no': {
+'category-adding': u'Robot: Legger til [[Kategori:%(newcat)s]]',
+'category-changing': u'Robot: Endrer %(oldcat)s',
+'category-was-moved': u'Robot: Kategorien ble flyttet til [[:Category:%(newcat)|%(title)s]]',
+'category-removing': u'Robot: Fjerner ifra %(oldcat)',
+'category-was-disbanded': u'Robot: Kategorien ble oppløst',
+'category-also-in': u'(også i %(alsocat))',
+},
+'fi': {
+'category-adding': u'Botti lisäsi luokkaan [[Luokka:%(newcat)s]]',
+'category-changing': u'Botti muutti luokan %(oldcat)s',
+'category-was-moved': u'Botti siirsi luokan nimelle [[:Luokka:%(newcat)|%(title)s]]',
+'category-listifying': u'Botti listasi luokan %(fromcat) (%(num)d jäsentä)',
+'category-removing': u'Botti poisti luokasta %(oldcat)',
+'category-was-disbanded': u'Botti tyhjensi luokan',
+'category-also-in': u'(myös luokassa %(alsocat))',
+},
+'uk': {
+'category-adding': u'Робот: додавання [[Категорія:%(newcat)s]]',
+'category-changing': u'Робот: зміна %(oldcat)s',
+'category-replacing': u'Робот: заміна категорії %(oldcat)s на %(newcat)s',
+'category-was-moved': u'Робот: категорію перейменовано на [[Категорія:%(newcat)|%(title)s]]',
+'category-removing': u'Робот: видалення з %(oldcat)',
+'category-was-disbanded': u'Робот: категорія розформована',
+'category-also-in': u'(також у %(alsocat))',
+},
+'cs': {
+'category-adding': u'Robot přidal [[Kategorie:%(newcat)s]]',
+'category-changing': u'Robot změnil [[%(oldcat)s]]→[[%(newcat)s]]',
+'category-replacing': u'Robot nahradil kategorii %(oldcat)s za %(newcat)s',
+'category-was-moved': u'Kategorie přesunuta na [[:Kategorie:%(newcat)|%(title)s]]',
+'category-listifying': u'Robot: vytvoření soupisu obsahu kategorie %(fromcat) (%(num)d položek)',
+'category-removing': u'Robot: odstranění kategorie %(oldcat)',
+'category-was-disbanded': u'Robot: kategorie byla vyprázdněna',
+'category-also-in': u'(také v %(alsocat))',
+},
+'hu': {
+'category-adding': u'[[Kategória:%(newcat)s]] hozzáadása bottal',
+'category-changing': u'Módosítás bottal: [[%(oldcat)s]]→[[%(newcat)s]]',
+'category-was-moved': u'A bot áthelyezte a kategória tartalmát ide: [[:Kategória:%(newcat)|%(title)s]]',
+'category-listifying': u'%(fromcat) listázása bottal (%(num)d lap)',
+'category-removing': u'[[%(oldcat)]] eltávolítása bottal',
+'category-was-disbanded': u'A bot kiürítette a kategóriát',
+},
+'ro': {
+},
+'tr': {
+},
+'ko': {
+'category-adding': u'로봇: [[분류:%(newcat)s]] 추가',
+'category-changing': u'로봇: %(oldcat)s 수정',
+'category-was-moved': u'로봇: 분류가 [[:분류:%(newcat)|%(title)s]]로 옮겨짐',
+},
+'vi': {
+},
+'id': {
+'category-adding': u'Bot: Menambahkan [[Kategori:%(newcat)s]]',
+'category-changing': u'Bot: Mengganti %(oldcat)s',
+'category-was-moved': u'Bot: Kategori dipindahkan ke [[:Category:%(newcat)|%(title)s]]',
+},
+'da': {
+'category-adding': u'Robot: Tilføjer [[Kategori:%(newcat)s]]',
+'category-changing': u'Robot: Ændrer %(oldcat)s',
+'category-was-moved': u'Robot: Kategori flyttet til [[:Category:%(newcat)|%(title)s]]',
+'category-removing': u'Robot: Fjerner fra %(oldcat)',
+'category-was-disbanded': u'Robot: Kategorien blev opløst',
+'category-also-in': u'(også i %(alsocat))',
+},
+'ar': {
+'category-adding': u'روبوت: إضافة [[تصنيف:%(newcat)s]]',
+'category-changing': u'روبوت: تغيير %(oldcat)s',
+'category-replacing': u'روبوت: استبدال التصنيف %(oldcat)s ب %(newcat)s',
+'category-was-moved': u'روبوت: التصنيف نقل إلى [[:تصنيف:%(newcat)|%(title)s]]',
+'category-listifying': u'روبوت: عرض من %(fromcat) (%(num)d مدخلة)',
+'category-removing': u'روبوت: إزالة من %(oldcat)',
+'category-was-disbanded': u'روبوت: التصنيف تم الاستغناء عنه',
+'category-also-in': u'(ﺄﻴﺿﺍ ﻒﻳ %(alsocat))',
+},
+'eo': {
+},
+'sr': {
+'category-adding': u'Бот: Додаје [[Категорија:%(newcat)s]]',
+'category-changing': u'Бот: Измена категорије %(oldcat)s',
+'category-was-moved': u'Бот: Категорија премештена у [[:Category:%(newcat)|%(title)s]]',
+'category-removing': u'Бот: Уклањање из категорије [[%(oldcat)]]',
+},
+'lt': {
+'category-adding': u'robotas: Pridedama [[Kategorija:%(newcat)s]]',
+'category-changing': u'robotas: Keičiama %(oldcat)s',
+'category-was-moved': u'robotas: Kategorija pervadinta į [[:Category:%(newcat)|%(title)s]]',
+},
+'sk': {
+'category-adding': u'Robot pridal [[Kategória:%(newcat)s]]',
+'category-changing': u'Robot zmenil [[%(oldcat)s]]→[[%(newcat)s]]',
+'category-replacing': u'Robot nahradil kategóriu %(oldcat)s za %(newcat)s',
+'category-was-moved': u'Kategória bola presunutá na [[:Kategória:%(newcat)|%(title)s]]',
+},
+'vo': {
+'category-adding': u'Bot: läükon: [[Klad:%(newcat)s]]',
+},
+'fa': {
+'category-adding': u'ربات: افزودن [[رده:%(newcat)s]]',
+'category-changing': u'ربات:تغییر %(oldcat)s',
+'category-replacing': u'ربات جایگزینی رده %(oldcat)s با %(newcat)s',
+'category-was-moved': u'ربات:رده به رده  [[:رده:%(newcat)|%(title)s]] منتقل شده‌است',
+'category-listifying': u'ربات:فهرست کردن اعضای رده %(fromcat)(%(num)d عضو)',
+'category-removing': u'ربات:حذف از %(oldcat)',
+'category-was-disbanded': u'ربات: رده خالی',
+'category-also-in': u'(ﻪﻣچﻥیﻥ ﺩﺭ %(alsocat))',
+},
+'he': {
+'category-adding': u'בוט: מוסיף [[קטגוריה:%(newcat)s]]',
+'category-changing': u'בוט: משנה %(oldcat)s',
+'category-was-moved': u'בוט: הקטגוריה הועברה לשם [[:קטגוריה:%(newcat)|%(title)s]]',
+'category-listifying': u'בוט: יוצר רשימה מהקטגוריה %(fromcat) (%(num)d דפים)',
+'category-removing': u'בוט: מסיר את הדף מהקטגוריה %(oldcat)',
+'category-was-disbanded': u'בוט: הקטגוריה פורקה',
+'category-also-in': u'(גם בקטגוריות %(alsocat))',
+},
+'bg': {
+},
+'sl': {
+},
+'war': {
+},
+'ms': {
+},
+'hr': {
+},
+'et': {
+},
+'new': {
+},
+'hi': {
+},
+'simple': {
+},
+'gl': {
+},
+'th': {
+},
+'eu': {
+},
+'nn': {
+'category-adding': u'robot: la til [[Kategori:%(newcat)s]]',
+'category-changing': u'robot: endra %(oldcat)s',
+'category-was-moved': u'robot: kategorien blei flytta til [[:Kategori:%(newcat)|%(title)s]]',
+'category-removing': u'robot: fjerna ifrå %(oldcat)',
+'category-was-disbanded': u'robot: kategorien blei løyst opp',
+'category-also-in': u'(òg i %(alsocat))',
+},
+'roa-rup': {
+},
+'el': {
+},
+'ht': {
+},
+'az': {
+},
+'tl': {
+},
+'la': {
+},
+'te': {
+},
+'ka': {
+},
+'mk': {
+},
+'ceb': {
+},
+'sh': {
+},
+'br': {
+},
+'pms': {
+},
+'mr': {
+},
+'lv': {
+},
+'lb': {
+'category-adding': u'Bot: Derbäi setzen [[Kategorie:%(newcat)s]]',
+'category-was-moved': u'Bot: Kategorie gouf gréckelt: Nei [[:Kategorie:%(newcat)|%(title)s]]',
+'category-removing': u'Bot: Ewech huele vun %(oldcat)',
+},
+'jv': {
+},
+'be-x-old': {
+'category-adding': u'Робат: дадаваньне [[Катэгорыя:%(newcat)s]]',
+'category-changing': u'Робат: зьмена %(oldcat)s',
+'category-was-moved': u'Робат: катэгорыя перайменаваная ў [[:Катэгорыя:%(newcat)|%(title)s]]',
+'category-removing': u'Робат: выключэньне з [[%(oldcat)]]',
+'category-was-disbanded': u'Робат: катэгорыя расфармаваная',
+'category-also-in': u'(таксама ў %(alsocat))',
+},
+'cy': {
+},
+'is': {
+'category-adding': u'Vélmenni: Bæti við [[Flokkur:%(newcat)s]]',
+'category-changing': u'Vélmenni: Breyti flokknum [[%(oldcat)s]]',
+'category-removing': u'Vélmenni: Fjarlægi [[%(oldcat)]]',
+'category-also-in': u'(einnig í %(alsocat))',
+},
+'bs': {
+},
+'sq': {
+},
+'ta': {
+},
+'be': {
+},
+'bpy': {
+},
+'an': {
+},
+'oc': {
+},
+'bn': {
+},
+'sw': {
+},
+'io': {
+},
+'fy': {
+},
+'lmo': {
+},
+'gu': {
+},
+'af': {
+},
+'nds': {
+'category-adding': u'Kat-Bot: [[Kategorie:%(newcat)s]] rin',
+'category-changing': u'Kat-Bot: %(oldcat)s utwesselt',
+'category-was-moved': u'Kat-Bot: Kategorie na [[:Category:%(newcat)|%(title)s]] schaven',
+'category-removing': u'Kat-Bot: rut ut %(oldcat)',
+'category-was-disbanded': u'Kat-Bot: Kategorie is nu oplööst',
+},
+'ml': {
+},
+'qu': {
+},
+'scn': {
+},
+'ur': {
+},
+'ku': {
+},
+'zh-yue': {
+},
+'su': {
+},
+'ast': {
+},
+'nap': {
+},
+'bat-smg': {
+'category-adding': u'Robots: Pridedama [[Kateguorėjė:%(newcat)s]]',
+'category-was-moved': u'Robots: Kateguorėjė bova parvadėnta i [[:Kateguorėjė:%(newcat)|%(title)s]]',
+'category-removing': u'Robots: Trėnama ėš %(oldcat)',
+},
+'hy': {
+},
+'yo': {
+},
+'ga': {
+},
+'cv': {
+},
+'wa': {
+},
+'ksh': {
+'category-adding': u'Bot: [[Saachjropp:%(newcat)s]] erinjedonn',
+'category-changing': u'Bot: %(oldcat)s ußjewääßelt',
+'category-was-moved': u'Bot: Saachjropp noh [[:Category:%(newcat)|%(title)s]] jeschovve',
+'category-removing': u'Bot: uß de %(oldcat) ußjedraare',
+'category-was-disbanded': u'Bot: de Saachjropp is nu opjelööß',
+},
+'ne': {
+},
+'pnb': {
+},
+'kn': {
+},
+'tg': {
+},
+'roa-tara': {
+},
+'vec': {
+},
+'yi': {
+},
+'gd': {
+},
+'tt': {
+},
+'zh-min-nan': {
+},
+'os': {
+},
+'uz': {
+},
+'pam': {
+},
+'als': {
+},
+'sah': {
+},
+'kk': {
+'category-adding': u'Бот: [[Санат:%(newcat)s]] үстеді',
+'category-changing': u'Бот: %(oldcat)s дегенді түзетті',
+'category-was-moved': u'Бот: Санат [[:Санат:%(newcat)|%(title)s]] дегенге жылжытылды',
+'category-listifying': u'Бот: %(fromcat) дегеннен (%(num)d буын) тізімдеді',
+'category-removing': u'Бот: %(oldcat) дегеннен аластатты',
+'category-was-disbanded': u'Бот: Санат тарқатылды',
+'category-also-in': u'(тағы да %(alsocat) дегенде)',
+},
+'arz': {
+},
+'mi': {
+},
+'li': {
+},
+'am': {
+},
+'nah': {
+},
+'hsb': {
+},
+'glk': {
+},
+'co': {
+},
+'gan': {
+},
+'mn': {
+},
+'sco': {
+},
+'ia': {
+'category-adding': u'Robot: Addition de [[Categoria:%(newcat)s]]',
+'category-changing': u'Robot: Modification de %(oldcat)s',
+'category-was-moved': u'Robot: Categoria transferite a [[:Category:%(newcat)|%(title)s]]',
+'category-removing': u'Robot: Eliminate de %(oldcat)',
+'category-was-disbanded': u'Robot: Categoria esseva dissolvite',
+'category-also-in': u'(equalmente in %(alsocat))',
+},
+'bcl': {
+},
+'my': {
+},
+'fo': {
+},
+'fiu-vro': {
+},
+'nds-nl': {
+'category-adding': u'bot: [[kattegerie:%(newcat)s]] derbie edaon',
+'category-changing': u'bot: wieziging %(oldcat)s',
+'category-was-moved': u'Bot: kattegerie is herneumd naor [[:Kattegerie:%(newcat)|%(title)s]]',
+'category-listifying': u'Bot: lieste van %(fromcat) (%(num)d pagina\'s)',
+'category-removing': u'Bot: vort-ehaold uut %(oldcat)',
+'category-was-disbanded': u'Bot: kattegerie besteet neet meer',
+'category-also-in': u'(oek in %(alsocat))',
+},
+'si': {
+},
+'tk': {
+},
+'vls': {
+},
+'sa': {
+},
+'bar': {
+},
+'mg': {
+},
+'gv': {
+},
+'ilo': {
+},
+'dv': {
+},
+'nrm': {
+},
+'pag': {
+},
+'map-bms': {
+},
+'rm': {
+},
+'ckb': {
+},
+'diq': {
+},
+'se': {
+},
+'mzn': {
+},
+'wuu': {
+},
+'fur': {
+},
+'ug': {
+},
+'lij': {
+},
+'mt': {
+},
+'bh': {
+},
+'nov': {
+},
+'bo': {
+},
+'ang': {
+},
+'csb': {
+},
+'sc': {
+},
+'zh-classical': {
+},
+'hif': {
+},
+'km': {
+},
+'lad': {
+},
+'cbk-zam': {
+},
+'pi': {
+},
+'frp': {
+},
+'ps': {
+},
+'hak': {
+},
+'kw': {
+},
+'pa': {
+},
+'nv': {
+},
+'szl': {
+'category-adding': u'Bot dodowo: [[Kategoria:%(newcat)s]]',
+},
+'xal': {
+},
+'pdc': {
+'category-adding': u'Waddefresser: [[Kategorie:%(newcat)s]] dezu geduh',
+'category-changing': u'Waddefresser: Abdeeling vun %(oldcat)s nooch %(newcat)s geennert',
+'category-replacing': u'Waddefresser: Abdeeling von %(oldcat)s nooch %(newcat)s geennert',
+'category-was-moved': u'Waddefresser: Abdeeling iss gezoge warre nooch [[:Kategorie:%(newcat)|%(title)s]].',
+},
+'haw': {
+},
+'stq': {
+},
+'ie': {
+},
+'so': {
+},
+'kv': {
+},
+'crh': {
+},
+'to': {
+},
+'ace': {
+},
+'mhr': {
+},
+'myv': {
+},
+'pcd': {
+},
+'krc': {
+},
+'ln': {
+},
+'ext': {
+},
+'gn': {
+},
+'ky': {
+},
+'eml': {
+},
+'arc': {
+},
+'pap': {
+},
+'ay': {
+},
+'rw': {
+},
+'jbo': {
+},
+'wo': {
+},
+'bjn': {
+},
+'frr': {
+},
+'ce': {
+},
+'ba': {
+},
+'kl': {
+},
+'tpi': {
+},
+'kab': {
+},
+'udm': {
+},
+'ty': {
+},
+'srn': {
+},
+'zea': {
+},
+'mrj': {
+},
+'koi': {
+},
+'dsb': {
+},
+'ig': {
+},
+'ab': {
+},
+'or': {
+},
+'gag': {
+},
+'kg': {
+},
+'lo': {
+},
+'av': {
+},
+'mdf': {
+},
+'mwl': {
+},
+'rmy': {
+},
+'cu': {
+},
+'kaa': {
+},
+'sm': {
+},
+'tet': {
+},
+'got': {
+},
+'ks': {
+},
+'bm': {
+},
+'ik': {
+},
+'sd': {
+},
+'bi': {
+},
+'lbe': {
+},
+'na': {
+},
+'pih': {
+},
+'iu': {
+},
+'pnt': {
+},
+'chr': {
+},
+'cdo': {
+},
+'as': {
+},
+'ss': {
+},
+'pfl': {
+},
+'ha': {
+},
+'ee': {
+},
+'ti': {
+},
+'om': {
+},
+'zu': {
+},
+'za': {
+},
+'ve': {
+},
+'ts': {
+},
+'rn': {
+},
+'dz': {
+},
+'sg': {
+},
+'cr': {
+},
+'ch': {
+},
+'lg': {
+},
+'bug': {
+},
+'bxr': {
+},
+'fj': {
+},
+'ak': {
+},
+'st': {
+},
+'xh': {
+},
+'ny': {
+},
+'tn': {
+},
+'ff': {
+},
+'ki': {
+},
+'tum': {
+},
+'sn': {
+},
+'tw': {
+},
+'chy': {
+},
+}
