5e12a84a37412267f62fb98aa9626ae3db2e44e9
amir <amir@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2011-02-10 00:14:09 +0000

    fixup for r8679
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk@8947 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- pywikipedia/catlib.py
+++ pywikipedia/catlib.py
@@ -458,11 +458,17 @@ class Category(wikipedia.Page):
             wikipedia.output('Moving text from %s to %s.' % (self.title(), targetCat.title()))
             authors = ', '.join(self.contributingUsers())
             creationSummary = wikipedia.translate(wikipedia.getSite(), msg_created_for_renaming) % (self.title(), authors)
-            #Maybe sometimes length of summary is more than 200 characters and thus will not be shown.so bot must listify authors in another place
+            #Maybe sometimes length of summary is more than 200 characters and thus will not be shown.
+            #For avoidning copyright violation bot must listify authors in another place
             if len(creationSummary)>200:
-                targetCat.put(self.get()+u"/n/nAuthors: %s" % authors, creationSummary)
-            else:
-                targetCat.put(self.get(), creationSummary)
+                talkpage=targetCat.toggleTalkPage()
+                try:
+                    talktext=talkpage.get()
+                except:
+                    talkpage.put(u"==Authors==\n%s-~~~~" % authors, u"Bot:Listifying authors")
+                else:
+                    talkpage.put(talktext+u"\n==Authors==\n%s-~~~~" % authors, u"Bot:Listifying authors")
+            targetCat.put(self.get(), creationSummary)
             return True
 
     #Like copyTo above, except this removes a list of templates (like deletion templates) that appear in
