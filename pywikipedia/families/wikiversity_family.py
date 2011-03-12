489119b0ed2a80031c7c1e3b056cb3ef2a90a5b4
xqt <xqt@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2011-03-12 23:09:14 +0000

    update language_by_size
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk@9036 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- pywikipedia/families/wikiversity_family.py
+++ pywikipedia/families/wikiversity_family.py
@@ -11,7 +11,7 @@ class Family(family.Family):
         self.name = 'wikiversity'
 
         self.languages_by_size = [
-            'en', 'fr', 'ru', 'de', 'cs', 'beta', 'es', 'it', 'pt', 'el', 'fi',
+            'en', 'fr', 'ru', 'cs', 'beta', 'de', 'pt', 'es', 'it', 'el', 'fi',
             'sv', 'ja',
         ]
 
