b1d0614173e2db5a845b614d010cf5550f4b57d5
jayvdb <jayvdb@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2011-04-25 15:02:45 +0000

    Add three unittests that demonstrate replaceCategoryLinks() does not support round-trips - instead it removes the last eol
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk@9199 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- pywikipedia/tests/test_textlib.py
+++ pywikipedia/tests/test_textlib.py
@@ -5,16 +5,21 @@
 __version__ = '$Id$'
 
 import unittest
-import tests.test_pywiki
+from tests.test_pywiki import PyWikiTestCase
 
 import wikipedia as pywikibot
 import pywikibot.textlib as textlib
 import catlib
 
 
-class PyWikiTextLibTestCase(tests.test_pywiki.PyWikiTestCase):
+class PyWikiTextLibTestCase(PyWikiTestCase):
 
     result1 = '[[Category:Cat1]]\r\n[[Category:Cat2]]\r\n'
+
+    def setUp(self):
+        self.site = pywikibot.getSite('en', 'wikipedia')
+        self.data = [catlib.Category(self.site, 'Category:Cat1'),
+                     catlib.Category(self.site, 'Category:Cat2')]
 
     def test_categoryFormat_raw(self):
         self.assertEqual(self.result1,
@@ -27,14 +32,32 @@ class PyWikiTextLibTestCase(tests.test_pywiki.PyWikiTestCase):
                          textlib.categoryFormat(['Cat1', 'Cat2'], self.site))
 
     def test_categoryFormat_Category(self):
-        data = [catlib.Category(self.site, 'Category:Cat1'),
-                catlib.Category(self.site, 'Category:Cat2')]
-        self.assertEqual(self.result1, textlib.categoryFormat(data, self.site))
+        self.assertEqual(self.result1,
+                         textlib.categoryFormat(self.data, self.site))
 
     def test_categoryFormat_Page(self):
         data = [pywikibot.Page(self.site, 'Category:Cat1'),
                 pywikibot.Page(self.site, 'Category:Cat2')]
-        self.assertEqual(self.result1, textlib.categoryFormat(data, self.site))
+        self.assertEqual(self.result1, textlib.categoryFormat(self.data,
+                                                              self.site))
+
+    def assertEqualCategoryRoundtrip(self, text, catcount):
+        cats = textlib.getCategoryLinks(text)
+        self.assertEqual(len(cats), catcount)
+        self.assertEqual(text, textlib.replaceCategoryLinks(text,
+                                                            cats,
+                                                            site = self.site))
+
+    def test_replaceCategoryLinks(self):
+        self.assertEqualCategoryRoundtrip(self.result1,2)
+
+    def test_replaceCategoryLinks1(self):
+        result = 'Blah\r\n\r\n[[Category:Cat1]]\r\n[[Category:Cat2]]\r\n'
+        self.assertEqualCategoryRoundtrip(result,2)
+
+    def test_replaceCategoryLinks2(self):
+        result = 'Blah\r\n\r\n[[Category:Cat1]]\r\n[[Category:Cat2]]\r\n\r\n[[fr:Test]]\r\n'
+        self.assertEqualCategoryRoundtrip(result,2)
 
 if __name__ == "__main__":
     unittest.main()
