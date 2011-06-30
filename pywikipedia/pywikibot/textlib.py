4f73fecaf95bbdebe3dcf557975a236ce64f6db4
xqt <xqt@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2011-06-30 12:40:56 +0000

    doc for replaceExcept() method changed. param old may also be a uncompiled regex. (bug reported by Bináris)
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk@9323 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- pywikipedia/pywikibot/textlib.py
+++ pywikipedia/pywikibot/textlib.py
@@ -42,7 +42,7 @@ def replaceExcept(text, old, new, exceptions, caseInsensitive=False,
 
     Parameters:
         text            - a unicode string
-        old             - a compiled regular expression
+        old             - a compiled od uncompiled regular expression
         new             - a unicode string (which can contain regular
                           expression references), or a function which takes
                           a match object as parameter. See parameter repl of
