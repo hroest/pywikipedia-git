60e7bfd118cec65aaab7ce9049d7c15f4ad3ec28
xqt <xqt@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2011-07-24 17:02:43 +0000

    update namespaces for ka-wiki
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk@9421 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- pywikipedia/families/wikipedia_family.py
+++ pywikipedia/families/wikipedia_family.py
@@ -61,8 +61,6 @@ class Family(family.Family):
         self.namespaces[13]['ja'] = [u'Help‐ノート', u'ヘルプ・トーク']
         self.namespaces[14]['ja'] = [u'Category', u'カテゴリ']
         self.namespaces[15]['ja'] = [u'Category‐ノート', u'カテゴリ・トーク']
-        self.namespaces[2]['ka'] = [u'ვიკიპედიელი', u'მომხმარებელი']
-        self.namespaces[3]['ka'] = [u'ვიკიპედიელის განხილვა', u'მომხმარებელი განხილვა', u'მომხმარებლის განხილვა']
         self.namespaces[2]['pl'] = u'Wikipedysta'
         self.namespaces[3]['pl'] = u'Dyskusja wikipedysty'
         self.namespaces[2]['pt'] = u'Usuário'
