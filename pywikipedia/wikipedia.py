hroest/pywikipedia-git

commit 0045258381b50b055b9070f3923dc305acfdda60

revert r5626 due to bug #2989218
git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk@9490 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

pywikipedia/wikipedia.py
@@ -435,8 +435,8 @@ not supported by PyWikipediaBot!"""
         """Return the character encoding used on this Page's wiki Site."""
         return self._site.encoding()
 
-    def title(self, underscore=False, savetitle=False, decode=False,
-              withNamespace=True,
+    @deprecate_arg("decode", None)
+    def title(self, underscore=False, savetitle=False, withNamespace=True,
               withSection=True, asUrl=False, asLink=False,
               allowInterwiki=True, forceInterwiki=False, textlink=False,
               as_filename=False):
@@ -460,16 +460,6 @@ not supported by PyWikipediaBot!"""
         If decode is True, decodes the section title
         """
         title = self._title
-        if decode or asLink:
-            begin = title.find('#')
-            if begin != -1:
-                anchor = self.section(underscore=underscore, decode=True)
-                try:
-                    title = title[:begin + 1] + anchor
-                except TypeError:
-                    print title, begin, anchor
-                    raise
-
         if asLink:
             iw_target_site = getSite()
             iw_target_family = getSite().family
@@ -517,18 +507,16 @@ not supported by PyWikipediaBot!"""
             result = result.replace(forbiddenChar, '_')
         return result
 
-    def section(self, underscore = False, decode=False):
+    @deprecate_arg("decode", None)
+    def section(self, underscore = False):
         """Return the name of the section this Page refers to.
 
         The section is the part of the title following a '#' character, if any.
         If no section is present, return None.
         """
         section = self._section
-        if section and decode:
-            section = section.replace('.', '%')
-            section = url2unicode(section, self._site)
-            if not underscore:
-                section = section.replace('_', ' ')
+        if underscore:
+            section = section.replace(' ', '_')
         return section
 
     def sectionFreeTitle(self, underscore=False):

commit 404f6db0ba4571caf5c258518055b5ae43534b66

do not replace section if it is None; fix for bug #3412831 introduced with r9490
git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk@9541 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

pywikipedia/wikipedia.py
@@ -510,7 +510,7 @@ not supported by PyWikipediaBot!"""
         If no section is present, return None.
         """
         section = self._section
-        if underscore:
+        if section and underscore:
             section = section.replace(' ', '_')
         return section
 