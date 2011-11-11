eaca5266446c3f10d7321049625f2865f0a49f03
xqt <xqt@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2011-11-11 02:33:55 +0000

    doi known_family error, bug #3436114
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk@9744 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- pywikipedia/family.py
+++ pywikipedia/family.py
@@ -3165,7 +3165,7 @@ class Family:
             'dmoz':             'dmoz',
             'dmozs':            'dmozs',
             'docbook':          'docbook',
-            'doi':              'doi',
+##            'doi':              'doi',
             'doom_wiki':        'doom_wiki',
             'download':         'download',
             'drae':             'drae',
