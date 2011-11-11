4d6c934af75d7f59d034974dc2fb700cfc78eaa9
xqt <xqt@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2011-11-11 03:28:35 +0000

    translations namespace
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk@9748 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- pywikipedia/families/test_family.py
+++ pywikipedia/families/test_family.py
@@ -139,6 +139,12 @@ class Family(family.Family):
         self.namespaces[133] = {
             '_default': u'نقاش المساعدة',
         }
+        self.namespaces[1198] = {
+            '_default': u'Translations',
+        }
+        self.namespaces[1199] = {
+            '_default': u'Translations talk',
+        }
         self.interwiki_forward = 'wikipedia'
         self.cross_projects = [
             'wikipedia', 'wiktionary', 'wikibooks', 'wikiquote', 'wikisource', 'wikinews', 'wikiversity',
